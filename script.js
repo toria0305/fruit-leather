d54ad7f48f4ae93b64eaed69d253e38c626a6af8
Victoria Maggay <toria0305@gmail.com>
2022-10-24 13:46:26 -0400

    Add an event

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+
+function addingEventListener() {
+    const button = document.getElementById('link');
+    console.log(button);
+
+    button.addEventListener('click',clickALert);
+
+    function clickALert() {
+        alert('Loading More Rugs!');
+    }
+}
+
+addingEventListener();
+
+
 const h2 = document.createElement("h2");
-h2.textContent = "INSTAGRAM: @FRUIT_LEATHER_RUGS";
+h2.textContent = "Thank You, Come Again!";
 
 document.querySelector("body").appendChild(h2);
